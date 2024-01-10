c3d256762977db7c3357817f43e64e608b89df91
jamalesmaily <jimi.esmaily@gmail.com>
2024-01-10 10:03:52 +0100

    add from mac

--- Visulase.py
+++ Visulase.py
@@ -1,15 +1,38 @@
 
 from rdkit import Chem
 from rdkit.Chem import Draw
+from dataset import *
+from torch_geometric.data import DataLoader
+import matplotlib.pyplot as plt
+
+train_dataset = MoleculeDataset(root="data/", filename='train_bace.csv')
+# test_dataset = MoleculeDataset(root="data/", filename="HIV_test.csv", test=True)
+# params["model_edge_dim"] = train_dataset[0].edge_attr.shape[1]
+
+# Prepare training
+train_loader = DataLoader(train_dataset, batch_size=len(train_dataset), shuffle=True)
+# test_loader = DataLoader(test_dataset, batch_size=params["batch_size"], shuffle=True)
 
 # define the smiles string and covert it into a molecule sturcture ------------
-caffeine_smiles = 'CN1C=NC2=C1C(=O)N(C(=O)N2C)C'
+sample_num = 100
 
-mol = Chem.MolFromSmiles(caffeine_smiles)
+sample_smile = train_dataset[sample_num].smiles
+mol = Chem.MolFromSmiles(sample_smile)
 
 # draw the modecule -----------------------------------------------------------
-Draw.MolToFile(mol, 'caffeine.png')
+Draw.MolToFile(mol, 'mol.png')
 
 # draw the molecule with property ---------------------------------------------
 for i, atom in enumerate(mol.GetAtoms()):
     atom.SetProp("molAtomMapNumber", str(atom.GetIdx()))
+
+Draw.MolToFile(mol, 'mol_num.png')
+
+
+##
+
+for _, batch in enumerate(tqdm(train_loader)):
+    a = 1
+    plt.hist(batch.y)
+
+plt.show()
--- dataset.py
+++ dataset.py
@@ -38,14 +38,36 @@ class MoleculeDataset(Dataset):
         self.data = pd.read_csv(self.raw_paths[0]).reset_index()
         featurizer = dc.feat.MolGraphConvFeaturizer(use_edges=True)
         for index, row in tqdm(self.data.iterrows(), total=self.data.shape[0]):
-            # Featurize molecule
             mol = Chem.MolFromSmiles(row['mol'])
-            f = featurizer._featurize(mol)
-            data = Data(x=torch.from_numpy(f.node_features).float())
-            # data = f.to_pyg_graph()
-            data.y = row["Class"]
-            data.smiles = row["mol"]
-            data.molfeature = row.iloc[6:20]
+
+            # Get node features
+            # torch.from_numpy(f.node_features).float()
+            node_feats = torch.from_numpy(featurizer._featurize(mol).node_features).float()
+            # Get edge features
+            edge_feats = self._get_edge_features(mol)
+            # Get adjacency info
+            edge_index = self._get_adjacency_info(mol)
+            # Get labels info
+            label = self._get_labels(row["Class"])
+
+            molfeatures = self._get_mol_features(row.iloc[6:20])
+
+            # Create data object
+            data = Data(x=node_feats,
+                        edge_index=edge_index,
+                        edge_attr=edge_feats,
+                        y=label,
+                        smiles=row["mol"],
+                        molfeature=molfeatures
+                        )
+            # Featurize molecule
+            # mol = Chem.MolFromSmiles(row['mol'])
+            # f = featurizer._featurize(mol)
+            # data = Data(x=torch.from_numpy(f.node_features).float())
+            # # data = f.to_pyg_graph()
+            # data.y = row["Class"]
+            # data.smiles = row["mol"]
+            # data.molfeature = row.iloc[6:20]
             if self.test:
                 torch.save(data,
                            os.path.join(self.processed_dir,
@@ -58,6 +80,50 @@ class MoleculeDataset(Dataset):
     # def _get_label(self, label):
     #     label = np.asarray([label])
     #     return torch.tensor(label, dtype=torch.int64)
+
+    def _get_edge_features(self, mol):
+        """
+        This will return a matrix / 2d array of the shape
+        [Number of edges, Edge Feature size]
+        """
+        all_edge_feats = []
+
+        for bond in mol.GetBonds():
+            edge_feats = []
+            # Feature 1: Bond type (as double)
+            edge_feats.append(bond.GetBondTypeAsDouble())
+            # Feature 2: Rings
+            edge_feats.append(bond.IsInRing())
+            # Append node features to matrix (twice, per direction)
+            all_edge_feats += [edge_feats, edge_feats]
+
+        all_edge_feats = np.asarray(all_edge_feats)
+        return torch.tensor(all_edge_feats, dtype=torch.float)
+
+    def _get_adjacency_info(self, mol):
+        """
+        We could also use rdmolops.GetAdjacencyMatrix(mol)
+        but we want to be sure that the order of the indices
+        matches the order of the edge features
+        """
+        edge_indices = []
+        for bond in mol.GetBonds():
+            i = bond.GetBeginAtomIdx()
+            j = bond.GetEndAtomIdx()
+            edge_indices += [[i, j], [j, i]]
+
+        edge_indices = torch.tensor(edge_indices)
+        edge_indices = edge_indices.t().to(torch.long).view(2, -1)
+        return edge_indices
+
+    def _get_labels(self, label):
+        label = np.asarray([label])
+        return torch.tensor(label, dtype=torch.int64)
+
+    def _get_mol_features(self, molfeatures):
+        # molfeatures = np.asarray([molfeatures])
+        return torch.tensor(molfeatures, dtype=torch.int64)
+
 
     def len(self):
         return self.data.shape[0]
